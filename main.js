e159eaa0572e6266c8b0088e5e3097c56fc0f413
Hatem Elnemr <thebillionairemindset2023@gmail.com>
2025-09-10 20:24:41 +0300

    Fixed FAQ-icon issue

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ faqQuestions.forEach((question) => {
       if (q !== question) {
         q.nextElementSibling.style.display = "none";
         const icon = q.querySelector(".faq-icon .icon");
-        icon.src = "images/icon-plus.svg";
+        icon.src = "./images/icon-plus.svg";
       }
     }
     answer.style.display = answer.style.display === "block" ? "none" : "block";
     if (icon.src.includes("images/icon-plus.svg")) {
-      icon.src = "images/icon-minus.svg";
+      icon.src = "./images/icon-minus.svg";
     } else {
-      icon.src = "images/icon-plus.svg";
+      icon.src = "./images/icon-plus.svg";
     }
   });
 });
